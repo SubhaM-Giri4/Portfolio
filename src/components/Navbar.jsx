6883049bb8b6c6bee9526bb94e8f6be6e834a453
Subham Kumar Giri <Subhamgiri482@gmail.com>
2026-02-12 23:59:25 +0530

    Added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ return(
   <div className='flex gap-6 items-center'>
     <a href='#projects' className='opacity-80 hover:opacity-100'>Projects</a>
     <a href='#contact' className='opacity-80 hover:opacity-100'>Contact</a>
-    <a href='https://drive.google.com/file/d/placeholder/view' className='px-3 py-1 bg-white dark:bg-black dark:text-white text-black border rounded-md'>Resume</a>
+    <a href='https://drive.google.com/file/d/1YzGe8TgCRqh_PNpgyQOY0cxxe422XgyW/view?usp=drive_link' className='px-3 py-1 bg-white dark:bg-black dark:text-white text-black border rounded-md'>Resume</a>
   </div>
 </div>);
 }
