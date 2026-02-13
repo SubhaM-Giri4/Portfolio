845774969023f659687f602cbb5d8e53a51292e8
Subham Kumar Giri <Subhamgiri482@gmail.com>
2026-02-13 23:48:12 +0530

    front label

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import Reveal from './Reveal';
+export default function Hero(){
+return(
+<Reveal>
+<div className='w-full max-w-5xl mt-20 text-center'>
+<h1 className='text-6xl font-extrabold'>
+  Hi, I'm <span className='text-purple-500'>Subham Kumar Giri</span>
+</h1>
+<p className='text-xl mt-4 opacity-80'>Crafting clean code and solving real-world problems</p>
+</div>
+</Reveal>);
+}
